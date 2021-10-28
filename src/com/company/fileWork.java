f0ef5b5cc47b18542ef043a5735095c4206cf90f
egeronik <eshik2002@gmail.com>
2021-10-28 16:01:57 +0300

    Final Patch

--- src/com/company/fileWork.java
+++ src/com/company/fileWork.java
@@ -1,16 +1,14 @@
 package com.company;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.util.Scanner;
 import java.io.*;
 import java.util.zip.*;
 
 
 public class fileWork {
-    static private String fileSysName = "sus.zip";
-    private String parent=null;
+    final static private String fileSysName = "sus.zip";
+    private String parent="";
 
     private void cat(String fileName){
         boolean exists=false;
@@ -18,8 +16,11 @@ public class fileWork {
             ZipInputStream  zin = new ZipInputStream(new FileInputStream(fileSysName));
             ZipEntry entry;
             while((entry = zin.getNextEntry()) != null){
+                String clearName;
+                if(entry.getName().lastIndexOf('/')==-1) clearName=entry.getName();
+                else clearName = entry.getName().substring(entry.getName().lastIndexOf('/')+1);
+                if(clearName.equals(fileName)){
 
-                if(entry.getName().equals(fileName)){
                     exists=true;
                     break;
                 }
@@ -29,6 +30,7 @@ public class fileWork {
                 System.out.println("File not exist");
                 return;
             }
+
             for (int c = zin.read(); c != -1; c = zin.read()) {
                 System.out.print((char)(c));
             }
@@ -45,8 +47,12 @@ public class fileWork {
             ZipEntry entry;
             while ((entry = zin.getNextEntry()) != null) {
                 File file = new File(entry.getName());
-                if(file.getParent()==parent)
+                if(file.getParent()==null&&parent.equals("")) System.out.println(entry.getName());
+                else if(file.getParent()!=null&&file.getParent().equals(parent))
+                   // if(entry.getName().lastIndexOf('/')!=-1) System.out.println(entry.getName().substring(entry.getName().lastIndexOf('/'),entry.getName().length()));
                     System.out.println(entry.getName());
+                //System.out.println(' ' + file.getParent());
+
 
             }
 
@@ -56,22 +62,59 @@ public class fileWork {
         }
     }
 
+    private void cd(String directoryName){
+        if(directoryName.equals("..")){
+            if(parent.lastIndexOf('\\')==-1) parent="";
+            else parent=parent.substring(0,parent.lastIndexOf('\\'));
+            return;
+        }
+        if(!parent.equals("")) directoryName= parent+'/'+directoryName;
+        try {
+            boolean exitsts = false;
+            ZipInputStream zin = new ZipInputStream(new FileInputStream(fileSysName));
+            ZipEntry entry;
+            while ((entry = zin.getNextEntry()) != null) {
+
+                if (entry.getName().equals(directoryName) ){
+                    directoryName = directoryName.replace('/','\\');
+                    directoryName = directoryName.substring(0,directoryName.length()-1);
+                    parent =  directoryName;
+                    return;
+                }
+
+
+            }
+            System.out.println("Directory not exists");
+            zin.closeEntry();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+
+
+
+    }
+
+    private void pwd(){
+        System.out.println(parent);
+    }
+
     public void initSystem()  {
 
         String input;
         Scanner scanner = new Scanner(System.in);
         input = scanner.next();
-        while(input!="exit"){
-            switch (input){
-                case "cat":
+        while(!input.equals("exit")){
+            switch (input) {
+                case "cat" -> {
                     input = scanner.next();
                     cat(input);
-                    break;
-                case "ls":
-                    ls();
-                    break;
-                case "exit":
-                    return;
+                }
+                case "ls" -> ls();
+                case "cd" -> {
+                    input = scanner.next();
+                    cd(input);
+                }
+                case "pwd" -> pwd();
             }
             input= scanner.next();
         }
